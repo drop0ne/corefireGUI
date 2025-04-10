002835f86b017d4034b11a53dcc92d2dc64966dc
Jacob Dougherty <core_fire@hotmail.com>
2025-04-10 00:48:39 -0500

    We have a window

--- corefireGUI/App.cpp
+++ corefireGUI/App.cpp
@@ -1,1 +1,12 @@
-#include "App.h"
+#include "App.h"  
+#include "MainFrame.h"  
+#include "wx/wx.h" 
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()  
+{
+	MainFrame* mainFrame = new MainFrame("Corefire GUI");
+   mainFrame->Show();  
+   return true;  
+}
--- corefireGUI/App.h
+++ corefireGUI/App.h
@@ -1,5 +1,9 @@
 #pragma once
-class App
+#include "wx/wx.h"
+
+class App : public wxApp
 {
+public:
+	virtual bool OnInit();
 };
 
