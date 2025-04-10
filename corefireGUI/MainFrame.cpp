9c3c74185e71372281aede5b2fd572170a2f897a
Jacob Dougherty <core_fire@hotmail.com>
2025-04-10 01:09:44 -0500

    Added button

--- corefireGUI/MainFrame.cpp
+++ corefireGUI/MainFrame.cpp
@@ -3,6 +3,9 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title)
 {
+	wxPanel* panel = new wxPanel(this);
+
+	wxButton* button = new wxButton(this, wxID_ANY, "Click Me", wxPoint(150, 50), wxSize(100,35));
 }
 
 MainFrame::~MainFrame()
