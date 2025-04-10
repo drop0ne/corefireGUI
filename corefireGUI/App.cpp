433db152eefad78a6bef301e73f9a5bef7088be6
Jacob Dougherty <core_fire@hotmail.com>
2025-04-10 01:01:30 -0500

    App.cpp mainFrame has default size and posistion

--- corefireGUI/App.cpp
+++ corefireGUI/App.cpp
@@ -7,6 +7,8 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit()  
 {
 	MainFrame* mainFrame = new MainFrame("Corefire GUI");
-   mainFrame->Show();  
-   return true;  
+	mainFrame->SetClientSize(800, 600);
+	mainFrame->Center();
+	mainFrame->Show();  
+	return true;  
 }
